a980b05346d9f628658f302f8f1cff7efd4749c1
DarkHorse786 <samiullahwaheed786@gmail.com>
2025-05-19 17:50:22 +0500

    Initial commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { Routes,Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+
+function App() {
+  return (
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain min-h-screen">
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/profile" element={<ProfilePage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ Client/src/components/RightSideBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RightSideBar = () => {
+  return (
+    <div>
+        <h1>Right side bar</h1>
+      
+    </div>
+  )
+}
+
+export default RightSideBar
