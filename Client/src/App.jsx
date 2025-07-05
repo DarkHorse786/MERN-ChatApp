7383fe277b037779974dbe160afa9c67459001ad
DarkHorse786 <samiullahwaheed786@gmail.com>
2025-07-05 20:21:53 +0500

    routes are protected

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,18 +1,21 @@
 import "./App.css";
-import { Routes,Route } from "react-router-dom";
+import { Navigate, Routes,Route } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import {Toaster} from "react-hot-toast";
+import { useContext } from "react";
+import { authContext } from "./context/AuthContext.jsx";
 
 function App() {
+  const {authUser} = useContext(authContext);
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain min-h-screen">
       <Toaster position="top-right" reverseOrder={false} />
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/profile" element={<ProfilePage/>} />
-        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/" element={authUser? <HomePage/> : <Navigate to="/login"/>}/>
+        <Route path="/profile" element={authUser? <ProfilePage/> : <Navigate to="/login"/>} />
+        <Route path="/login" element={!authUser? <LoginPage/> : <Navigate to="/" />} />
       </Routes>
     </div>
   );
