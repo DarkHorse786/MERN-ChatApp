2ad6a112980715616bfe18bb864c373576f07260
DarkHorse786 <samiullahwaheed786@gmail.com>
2025-07-15 16:59:57 +0500

    profile page is created

--- Client/src/components/RightSideBar.jsx
+++ Client/src/components/RightSideBar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import assets, { imagesDummyData } from "../assets/assets";
+import { useContext } from "react";
+import { ChatContext } from "../context/ChatContext.jsx";
+import { authContext } from "../context/AuthContext.jsx";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const RightSideBar = ({ selectedUser }) => {
+const RightSideBar = () => {
+  const { selectedUser, messages } = useContext(ChatContext);
+  const {onlineUsers} = useContext(authContext);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    if (selectedUser) {
+      setImages(messages.filter(msg => msg.image).map(msg => msg.image));
+    }
+  }, [messages, selectedUser]);
+
   return (
     selectedUser && (
       <div
@@ -11,12 +26,12 @@ const RightSideBar = ({ selectedUser }) => {
       >
         <div className="pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto">
           <img
-            src={selectedUser?.profilePic || assets.avatar_icon}
+            src={selectedUser?.profilePic || assets.profile_martin}
             alt=""
             className="w-20 aspect-[1/1] rounded-full"
           />
           <h1 className="px-10 text-xl font-medium mx-auto flex items-center gap-2">
-            <p className="w-2 h-2 rounded-full bg-green-500"></p>
+            {onlineUsers.includes(selectedUser._id) && <p className="w-2 h-2 rounded-full bg-green-500"></p>}
             {selectedUser.fullName}
           </h1>
           <p className="px-10 mx-auto">{selectedUser.bio}</p>
@@ -25,7 +40,7 @@ const RightSideBar = ({ selectedUser }) => {
         <div className="px-5 text-xs ">
           <p>Media</p>
           <div className="mt-2 mx-h-[200px] overflow-x-scroll grid grid-cols-2 gap-4 opacity-80">
-            {imagesDummyData.map((url, index) => (
+            {images.map((url, index) => (
               <div
                 className="cursor-pointer hover:scale-105 transition-transform duration-200"
                 key={index}
